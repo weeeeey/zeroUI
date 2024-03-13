ed141b80374bae78b4adcc8b3ba1a8f850e99066
weeeeey <qser155@naver.com>
2024-03-14 00:49:30 +0900

    spolight btn - mix-blend-difference && left

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { GradientBtn } from '@/components/ui/buttons/gradient-btn';
+import { SpotlightBtn } from '@/components/ui/buttons/spotlight-btn';
 
 const MainPage = () => {
     return (
         <div className="w-full h-full bg-sky-300 flex justify-center items-center px-20 py-10">
-            <GradientBtn />
+            <SpotlightBtn />
         </div>
     );
 };
---
+++ components/ui/buttons/spotlight-btn.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+
+export const SpotlightBtn = () => {
+    const [x, setX] = React.useState(0);
+    useEffect(() => {
+        console.log(x);
+    }, [x]);
+    return (
+        <button
+            onMouseMove={(e) => {
+                setX(
+                    ((e.clientX - e.currentTarget.offsetLeft) /
+                        e.currentTarget.offsetWidth) *
+                        100
+                );
+            }}
+            onMouseLeave={() => {
+                setX(50);
+            }}
+            className="bg-black flex justify-center items-center px-32 rounded-xl py-6 relative overflow-hidden"
+        >
+            <span className="text-white font-medium text-xl relative z-10 mix-blend-difference">
+                Hover me
+            </span>
+            <div
+                style={{
+                    left: `${x}%`,
+                }}
+                className="
+            left-[50%]
+            absolute w-32 h-32 rounded-full bg-white top-1/2  -translate-x-1/2 -translate-y-1/2"
+            />
+        </button>
+    );
+};
