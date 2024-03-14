99e7ad02ee288be667b434e40ea059841c8c4de3
zerojin <qser155@naver.com>
2024-03-14 13:04:24 +0900

    neubrutalism btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { DrawOutlineBtn } from '@/components/ui/buttons/draw-outline-btn';
+import { NeubrutalismBtn } from '@/components/ui/buttons/neubrutalism-btn';
 import { NeumorphismBtn } from '@/components/ui/buttons/neumorphism-btn';
 import { ShinyBtn } from '@/components/ui/buttons/shiny-btn';
 import { SpotlightBtn } from '@/components/ui/buttons/spotlight-btn';
 
 const MainPage = () => {
     return (
-        <div className="bg-black/90 w-full h-full  flex justify-center items-center px-20 py-10">
-            <DrawOutlineBtn />
+        <div className="bg-white w-full h-full  flex justify-center items-center px-20 py-10">
+            <NeubrutalismBtn />
         </div>
     );
 };
---
+++ components/ui/buttons/neubrutalism-btn.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const NeubrutalismBtn = () => {
+    return (
+        <button
+            className="px-4 py-2 bg-indigo-500 
+    -translate-x-1 -translate-y-1 shadow-[5px_5px_black] text-white
+    hover:translate-x-0 hover:translate-y-0 transition-all hover:shadow-none
+    "
+        >
+            Hover me
+        </button>
+    );
+};
