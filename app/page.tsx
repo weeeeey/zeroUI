e6f300a841cd547ebb3cf7b0d7628574ee2b69a8
zerojin <qser155@naver.com>
2024-03-15 13:39:16 +0900

    color change

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { Layout } from '@/components/ui/cards/color-change';
+import { ColorChageLayout } from '@/components/ui/cards/color-change';
 
 const MainPage = () => {
     return (
         <div className="bg-black w-full h-full  flex justify-center items-center px-20 py-10">
-            <Layout />
+            <ColorChageLayout />
         </div>
     );
 };
--- components/ui/cards/color-change.tsx
+++ components/ui/cards/color-change.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
 import { DUMMY_TEXT } from '.';
-import { ArrowRight } from 'lucide-react';
 
-export const Layout = () => {
+import { ArrowRight } from 'lucide-react';
+import { motion } from 'framer-motion';
+
+export const ColorChageLayout = () => {
     return (
         <div className="w-full h-full bg-white/80 p-10 flex flex-col space-y-10">
             <ColorChange decription={DUMMY_TEXT} title="Connect" imgSrc="3" />
@@ -22,12 +24,56 @@ export const ColorChange = ({
     imgSrc,
 }: ColorChangeProps) => {
     return (
-        <div className="w-full h-[30%] relative flex flex-col justify-end p-4 group ">
+        <motion.div
+            whileHover="hover"
+            className="cursor-pointer w-full h-[30%] relative flex flex-col justify-end p-4 group "
+        >
             <ArrowRight className="absolute z-10 top-5 right-5 w-7 h-7 group-hover:rotate-[-30deg] transition-all duration-500 text-slate-300" />
-            <div className="relative space-y-4 z-10">
-                <h1 className="text-white font-bold text-xl">{title}</h1>
+            <motion.div className="relative  z-10 ">
+                <motion.div className="text-white font-bold text-xl ">
+                    {Array.from(title).map((letter, index) => (
+                        <motion.span
+                            key={index}
+                            variants={{
+                                hover: {
+                                    translateY: '-50%',
+                                    opacity: 0,
+                                    transition: {
+                                        duration: 0.3,
+                                        delay: index * 0.05,
+                                    },
+                                },
+                            }}
+                            className="inline-block"
+                        >
+                            {letter}
+                        </motion.span>
+                    ))}
+                </motion.div>
+                <motion.div className="text-white font-bold text-xl ">
+                    {Array.from(title).map((letter, index) => (
+                        <motion.span
+                            initial={{ translateY: '0%', opacity: 0 }}
+                            variants={{
+                                hover: {
+                                    translateY: '-100%',
+                                    opacity: 1,
+                                    transition: {
+                                        duration: 0.3,
+                                        delay: index * 0.05,
+                                    },
+                                },
+                            }}
+                            key={index}
+                            className="inline-block"
+                        >
+                            {letter}
+                        </motion.span>
+                    ))}
+                </motion.div>
+
                 <p className="text-white">{decription}</p>
-            </div>
+            </motion.div>
 
             <div className="absolute top-0 left-0 w-full h-full saturate-100 overflow-hidden ">
                 <Image
@@ -39,6 +85,6 @@ export const ColorChange = ({
                 />
             </div>
             <div className="absolute top-0 left-0 w-full h-full bg-slate-800 opacity-60 transition-all duration-500 group-hover:bg-transparent" />
-        </div>
+        </motion.div>
     );
 };
